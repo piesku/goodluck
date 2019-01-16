56c63ce62b84c83327c13f5924dfdc0092117c0c
Staś Małolepszy <stas@piesku.com>
2019-01-16 21:13:42 +0100

    create_render

--- shape_cube.js
+++ shape_cube.js
@@ -1,5 +1,6 @@
 import * as mat4 from "./gl-matrix/mat4.js";
 import {COMPONENT_RENDER, COMPONENT_TRANSFORM} from "./components.js";
+import {create_render} from "./system_render.js";
 
 let vertex_array = Float32Array.from([
     -0.5, -0.5, -0.5, -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, -0.5, -0.5,
@@ -26,26 +27,7 @@ function create_cube(game, material, color) {
     let entity = game.create_entity(
             COMPONENT_TRANSFORM | COMPONENT_RENDER);
     game.components.transform[entity] = mat4.create();
-
-    let {gl} = game;
-    let vao = gl.createVertexArray()
-    gl.bindVertexArray(vao);
-
-    let vertex_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertex_array, gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(material.attribs.position);
-    gl.vertexAttribPointer(material.attribs.position, 3, gl.FLOAT, false,
-            3 * Float32Array.BYTES_PER_ELEMENT,
-            0 * Float32Array.BYTES_PER_ELEMENT);
-
-    let index_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, index_array, gl.STATIC_DRAW);
-
-    gl.bindVertexArray(null);
-    game.components.render[entity] = {
-            vao, count: index_array.length, material, color};
-
+    game.components.render[entity] = create_render(
+            game.gl, vertex_array, index_array, material, color);
     return entity;
 }
