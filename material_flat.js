3acf0f65ff5dd3fd946d64887db4a87759ca17c7
Staś Małolepszy <stas@piesku.com>
2019-01-17 01:16:08 +0100

    Flat material: Add static lighting

--- material_flat.js
+++ material_flat.js
@@ -10,16 +10,17 @@ let vertex = `#version 300 es
     flat out vec4 vert_color;
 
     const vec3 light_pos = vec3(-10.0, 10.0, 10.0);
+    const vec3 light_color = vec3(1.0, 1.0, 1.0);
 
     void main() {
         vec4 world_pos = model * vec4(position, 1.0);
         gl_Position = pv * world_pos;
 
-        vec3 world_normal = (model * vec4(normal, 1.0)).xyz;
+        vec3 world_normal = normalize((model * vec4(normal, 1.0)).xyz);
         vec3 light_dir = normalize(light_pos - world_pos.xyz);
         float light_factor = max(dot(world_normal, light_dir), 0.0);
 
-        vert_color = vec4(color.xyz * light_factor, 1.0);
+        vert_color = vec4(color.xyz * light_color * light_factor, 1.0);
     }
 `;
 
