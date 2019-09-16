68191decd31e3879f600b7bda558cd396e310bc6
Staś Małolepszy <stas@piesku.com>
2019-09-16 12:09:32 +0200

    (sys_camera) Remove Camera.Position

--- src/components/com_camera.ts
+++ src/components/com_camera.ts
@@ -1,10 +1,9 @@
 import {Entity, Game} from "../game.js";
-import {Mat4, Vec3} from "../math/index.js";
+import {Mat4} from "../math/index.js";
 import {create, perspective} from "../math/mat4.js";
 import {Get} from "./com_index.js";
 
 export interface Camera {
-    Position: Vec3;
     Projection: Mat4;
     View: Mat4;
     PV: Mat4;
@@ -14,7 +13,6 @@ export function camera(aspect: number, fovy: number, near: number, far: number) 
     return (game: Game) => (entity: Entity) => {
         game.World[entity] |= 1 << Get.Camera;
         game[Get.Camera][entity] = <Camera>{
-            Position: [],
             Projection: perspective(create(), fovy, aspect, near, far),
             View: create(),
             PV: create(),
--- src/systems/sys_camera.ts
+++ src/systems/sys_camera.ts
@@ -1,6 +1,6 @@
 import {Get} from "../components/com_index.js";
 import {Entity, Game} from "../game.js";
-import {get_translation, invert, multiply} from "../math/mat4.js";
+import {invert, multiply} from "../math/mat4.js";
 
 const QUERY = (1 << Get.Transform) | (1 << Get.Camera);
 
@@ -17,7 +17,6 @@ function update(game: Game, entity: Entity) {
     let transform = game[Get.Transform][entity];
     let camera = game[Get.Camera][entity];
     game.Cameras.push(camera);
-    get_translation(camera.Position, transform.World);
     invert(camera.View, transform.World);
     multiply(camera.PV, camera.Projection, camera.View);
 }
