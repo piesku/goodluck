4cec08e9dcacc8cfc03271448b93190e40f8da3d
Staś Małolepszy <stas@piesku.com>
2020-02-14 17:50:54 +0100

    Add Instanced

---
+++ Instanced/components/com_render.ts
@@ -0,0 +1,7 @@
+import {RenderInstanced} from "./com_render_instanced.js";
+
+export type Render = RenderInstanced;
+
+export const enum RenderKind {
+    Instanced,
+}
---
+++ Instanced/scenes/sce_stage.ts
@@ -0,0 +1,46 @@
+import {blueprint_camera} from "../blueprints/blu_camera.js";
+import {light} from "../components/com_light.js";
+import {render_instanced} from "../components/com_render_instanced.js";
+import {instantiate} from "../core.js";
+import {Game} from "../game.js";
+import {World} from "../world.js";
+
+export function scene_stage(game: Game) {
+    game.World = new World();
+    game.Cameras = [];
+    game.Lights = [];
+    game.GL.clearColor(1, 0.3, 0.3, 1);
+
+    // Camera.
+    instantiate(game, {
+        Translation: [2, 0, 5],
+        ...blueprint_camera(game),
+    });
+
+    // Light 1.
+    instantiate(game, {
+        Translation: [-2, 5, 5],
+        Using: [light([1, 1, 1], 3)],
+    });
+
+    // Light 2.
+    instantiate(game, {
+        Translation: [1, 3, 5],
+        Using: [light([1, 1, 1], 5)],
+    });
+
+    // A voxel model.
+    instantiate(game, {
+        Translation: [0, 0, 0],
+        Using: [
+            render_instanced(Float32Array.from([0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1]), [
+                1,
+                1,
+                0.3,
+                0.3,
+                1,
+                0.3,
+            ]),
+        ],
+    });
+}
