252d36d55a80ba7f2321d3cbcad16bba2b572d9d
Staś Małolepszy <stas@piesku.com>
2020-02-16 22:05:31 +0100

    Lint

--- src/sounds/snd_music.ts
+++ src/sounds/snd_music.ts
@@ -3,7 +3,20 @@ import {AudioClip} from "../components/com_audio_source";
 export let snd_music = <AudioClip>{
     Tracks: [
         {
-            Instrument: [7, , , , , , , , [["sine", 7, 1, 1, 4, 7], ["sine", 1, 1, 1, 6, 7]]],
+            Instrument: [
+                7,
+                ,
+                ,
+                ,
+                ,
+                ,
+                ,
+                ,
+                [
+                    ["sine", 7, 1, 1, 4, 7],
+                    ["sine", 1, 1, 1, 6, 7],
+                ],
+            ],
             Notes: [72, , , , 74, , , , 72],
         },
     ],
