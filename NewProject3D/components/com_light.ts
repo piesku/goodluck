c2ce0e0eace910b12d59a2317f763e57dc613053
Staś Małolepszy <stas@piesku.com>
2020-02-22 11:39:51 +0100

    (NewProject3D) game.LightPositions, game.LightDetails

--- NewProject3D/components/com_light.ts
+++ NewProject3D/components/com_light.ts
@@ -3,16 +3,14 @@ import {Entity, Game} from "../game.js";
 import {Has} from "./com_index.js";
 
 export interface Light {
-    EntityId: Entity;
     Color: Vec3;
     Intensity: number;
 }
 
 export function light(color: Vec3 = [1, 1, 1], range: number = 1) {
-    return (game: Game, EntityId: Entity) => {
-        game.World.Mask[EntityId] |= Has.Light;
-        game.World.Light[EntityId] = <Light>{
-            EntityId,
+    return (game: Game, entity: Entity) => {
+        game.World.Mask[entity] |= Has.Light;
+        game.World.Light[entity] = <Light>{
             Color: color,
             Intensity: range ** 2,
         };
