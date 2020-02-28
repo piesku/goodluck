9516b280bbc896e057c5d389dc8892ffb39c8516
Staś Małolepszy <stas@piesku.com>
2020-02-28 18:41:12 +0100

    Fixed hand mesh

--- meshes/blend2obj.py
+++ meshes/blend2obj.py
@@ -3,6 +3,13 @@ import sys
 
 # get all args after "--"
 argv = sys.argv[sys.argv.index("--") + 1:]
+
+# join all meshes
+bpy.ops.object.select_all(action="DESELECT")
+bpy.ops.object.select_by_type(type="MESH")
+bpy.ops.object.join()
+
+# export obj
 bpy.ops.export_scene.obj(
     filepath=argv[0],
     check_existing=False,
