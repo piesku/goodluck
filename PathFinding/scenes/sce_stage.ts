c8db3d4c0f69f9344d18e5d1fda129508392834f
Staś Małolepszy <stas@piesku.com>
2020-03-24 20:52:22 +0100

    Create the pathfinding graph using nested loops (results in duplicates in the list of adjacent nodes)

--- PathFinding/scenes/sce_stage.ts
+++ PathFinding/scenes/sce_stage.ts
@@ -34,4 +34,48 @@ export function scene_stage(game: Game) {
         Translation: [0, 0.3, 0],
         Using: [render_basic(game.MaterialBasicWireframe, game.MeshNavmesh, [1, 1, 0, 1])],
     });
+
+    let mesh = game.MeshNavmesh;
+
+    let vert2face: Record<number, Array<number>> = {};
+    for (let fi = 0; fi < mesh.IndexCount; fi += 3) {
+        for (let vi = 0; vi < 3; vi++) {
+            let v = mesh.IndexArray[fi + vi];
+            if (vert2face[v]) {
+                vert2face[v].push(fi);
+            } else {
+                vert2face[v] = [fi];
+            }
+        }
+    }
+
+    let graph: Record<number, Array<number>> = {};
+    for (let faceidx = 0; faceidx < mesh.IndexCount; faceidx += 3) {
+        graph[faceidx / 3] = [];
+        for (let vertidx = 0; vertidx < 3; vertidx++) {
+            let vertex = mesh.IndexArray[faceidx + vertidx];
+            for (let otherfaceidx of vert2face[vertex]) {
+                if (otherfaceidx === faceidx) {
+                    continue;
+                }
+                other: for (let othervertidx = 0; othervertidx < 3; othervertidx++) {
+                    let othervert = mesh.IndexArray[otherfaceidx + othervertidx];
+                    if (othervert === vertex) {
+                        continue other;
+                    }
+                    if (
+                        othervert === mesh.IndexArray[faceidx + 0] ||
+                        othervert === mesh.IndexArray[faceidx + 1] ||
+                        othervert === mesh.IndexArray[faceidx + 2]
+                    ) {
+                        graph[faceidx / 3].push(otherfaceidx / 3);
+                        break other;
+                    }
+                }
+            }
+        }
+    }
+
+    console.log(vert2face);
+    console.log(graph);
 }
