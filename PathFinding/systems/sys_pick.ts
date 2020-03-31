c9554b8c1632489ae4de11f503b09d0d6cea6e4f
Staś Małolepszy <stas@piesku.com>
2020-03-31 08:39:14 +0200

    Optimize Vec3 allocation

--- PathFinding/systems/sys_pick.ts
+++ PathFinding/systems/sys_pick.ts
@@ -82,30 +82,34 @@ interface PickingResult {
     Point: Vec3;
 }
 
+let K: Vec3 = [0, 0, 0];
+let L: Vec3 = [0, 0, 0];
+let M: Vec3 = [0, 0, 0];
+let E: Vec3 = [0, 0, 0];
+let F: Vec3 = [0, 0, 0];
+let G: Vec3 = [0, 0, 0];
+let N: Vec3 = [0, 0, 0];
+
 // Based on https://www.codeproject.com/Articles/625787/Pick-Selection-with-OpenGL-and-OpenCL
 function intersect_mesh(mesh: Mesh, origin: Vec3, direction: Vec3): PickingResult | null {
     let tri_count = mesh.IndexCount / 3;
 
     for (let tri = 0; tri < tri_count; tri++) {
-        let i1 = mesh.IndexArray[tri * 3 + 0];
-        let i2 = mesh.IndexArray[tri * 3 + 1];
-        let i3 = mesh.IndexArray[tri * 3 + 2];
+        let i1 = mesh.IndexArray[tri * 3 + 0] * 3;
+        let i2 = mesh.IndexArray[tri * 3 + 1] * 3;
+        let i3 = mesh.IndexArray[tri * 3 + 2] * 3;
 
-        let K: Vec3 = [
-            mesh.VertexArray[i1 * 3 + 0],
-            mesh.VertexArray[i1 * 3 + 1],
-            mesh.VertexArray[i1 * 3 + 2],
-        ];
-        let L: Vec3 = [
-            mesh.VertexArray[i2 * 3 + 0],
-            mesh.VertexArray[i2 * 3 + 1],
-            mesh.VertexArray[i2 * 3 + 2],
-        ];
-        let M: Vec3 = [
-            mesh.VertexArray[i3 * 3 + 0],
-            mesh.VertexArray[i3 * 3 + 1],
-            mesh.VertexArray[i3 * 3 + 2],
-        ];
+        K[0] = mesh.VertexArray[i1 + 0];
+        K[1] = mesh.VertexArray[i1 + 1];
+        K[2] = mesh.VertexArray[i1 + 2];
+
+        L[0] = mesh.VertexArray[i2 + 0];
+        L[1] = mesh.VertexArray[i2 + 1];
+        L[2] = mesh.VertexArray[i2 + 2];
+
+        M[0] = mesh.VertexArray[i3 + 0];
+        M[1] = mesh.VertexArray[i3 + 1];
+        M[2] = mesh.VertexArray[i3 + 2];
 
         // O + tD = kK + lL + mM
         // O + tD = kK + lL + (1 - k - l)M
@@ -114,12 +118,14 @@ function intersect_mesh(mesh: Mesh, origin: Vec3, direction: Vec3): PickingResul
         // O - M = k(K - M) + l(L - M) - tD
         // G = kE + lF - tD
 
-        // Two edges of the tri (to calculate the normal).
-        let E: Vec3 = subtract([0, 0, 0], K, M);
-        let F: Vec3 = subtract([0, 0, 0], L, M);
+        // Two edges of the tri: E, F.
+        subtract(E, K, M);
+        subtract(F, L, M);
 
-        // The third "edge" between M and the ray's origin.
-        let G: Vec3 = subtract([0, 0, 0], origin, M);
+        // The third "edge" between M and the ray's origin: G.
+        subtract(G, origin, M);
+
+        // From now on, M is used as a temporary Vec3.
 
         // Given the linear system of equations:
         //     kE + lF - tD = G
@@ -136,8 +142,8 @@ function intersect_mesh(mesh: Mesh, origin: Vec3, direction: Vec3): PickingResul
         //     l = D·(G×E) / D·(F×E)
         //     t = G·(E×F) / D·(F×E)
 
-        let normal = cross([0, 0, 0], F, E);
-        let denominator = dot(direction, normal);
+        cross(N, F, E);
+        let denominator = dot(direction, N);
         if (denominator >= 0) {
             // The tri's normal and the ray's direction are too similar.
             // The ray would intersect the tri from the back side.
@@ -146,7 +152,7 @@ function intersect_mesh(mesh: Mesh, origin: Vec3, direction: Vec3): PickingResul
 
         // k = D·(F×G) / D·(F×G). Don't divide by D·(F×G) to save cycles, and
         // flip the comparison to emulate the negative denomiator.
-        let k = dot(direction, cross([0, 0, 0], F, G));
+        let k = dot(direction, cross(M, F, G));
         if (k > 0) {
             // Barycentric coordinate < 0, no intersection.
             continue;
@@ -154,7 +160,7 @@ function intersect_mesh(mesh: Mesh, origin: Vec3, direction: Vec3): PickingResul
 
         // l = D·(G×E) / D·(F×G). Don't divide by D·(F×G) to save cycles, and
         // flip the comparison to emulate the negative denomiator.
-        let l = dot(direction, cross([0, 0, 0], G, E));
+        let l = dot(direction, cross(M, G, E));
         if (l > 0) {
             // Barycentric coordinate < 0, no intersection.
             continue;
@@ -168,10 +174,13 @@ function intersect_mesh(mesh: Mesh, origin: Vec3, direction: Vec3): PickingResul
         }
 
         // t = G·(E×F) / D·(F×G)
-        let t = dot(G, cross([0, 0, 0], E, F)) / denominator;
-        let intersection = scale([0, 0, 0], direction, t);
-        add(intersection, intersection, origin);
-        return {Tri: tri, Point: intersection};
+        // G·(E×F) = -G·(F×E) = -G·N
+        let t = -dot(G, N) / denominator;
+
+        // Intersection is O + tD.
+        scale(M, direction, t);
+        add(M, M, origin);
+        return {Tri: tri, Point: M};
     }
 
     return null;
