fe57cc5e109f1510103ca508c53bc04112925088
Staś Małolepszy <stas@piesku.com>
2020-05-20 09:47:42 +0200

    Add the RenderTexture example

--- common/texture.ts
+++ common/texture.ts
@@ -1,4 +1,17 @@
-import {GL_PIXEL_UNSIGNED_BYTE, GL_RGBA, GL_TEXTURE_2D} from "./webgl.js";
+import {
+    GL_CLAMP_TO_EDGE,
+    GL_DATA_UNSIGNED_BYTE,
+    GL_DATA_UNSIGNED_INT,
+    GL_DEPTH_COMPONENT,
+    GL_DEPTH_COMPONENT16,
+    GL_LINEAR,
+    GL_PIXEL_UNSIGNED_BYTE,
+    GL_RGBA,
+    GL_TEXTURE_2D,
+    GL_TEXTURE_MIN_FILTER,
+    GL_TEXTURE_WRAP_S,
+    GL_TEXTURE_WRAP_T,
+} from "./webgl.js";
 
 export function fetch_image(path: string): Promise<HTMLImageElement> {
     return new Promise((resolve) => {
@@ -13,5 +26,49 @@ export function create_texture_from(gl: WebGLRenderingContext, image: HTMLImageE
     gl.bindTexture(GL_TEXTURE_2D, texture);
     gl.texImage2D(GL_TEXTURE_2D, 0, GL_RGBA, GL_RGBA, GL_PIXEL_UNSIGNED_BYTE, image);
     gl.generateMipmap(GL_TEXTURE_2D);
+    return texture;
+}
+
+export function create_texture_rgba(gl: WebGLRenderingContext, width: number, height: number) {
+    let texture = gl.createTexture()!;
+    gl.bindTexture(GL_TEXTURE_2D, texture);
+    gl.texImage2D(
+        GL_TEXTURE_2D,
+        0,
+        GL_RGBA,
+        width,
+        height,
+        0,
+        GL_RGBA,
+        GL_DATA_UNSIGNED_BYTE,
+        null
+    );
+
+    gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    return texture;
+}
+
+export function create_texture_depth(gl: WebGLRenderingContext, width: number, height: number) {
+    let texture = gl.createTexture()!;
+    gl.bindTexture(GL_TEXTURE_2D, texture);
+    gl.texImage2D(
+        GL_TEXTURE_2D,
+        0,
+        GL_DEPTH_COMPONENT16,
+        width,
+        height,
+        0,
+        GL_DEPTH_COMPONENT,
+        GL_DATA_UNSIGNED_INT,
+        null
+    );
+
+    gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
     return texture;
 }
---
+++ meshes/plane.ts
@@ -0,0 +1,84 @@
+import {Mesh} from "../common/material.js";
+import {GL_ARRAY_BUFFER, GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW} from "../common/webgl.js";
+
+export function mesh_plane(gl: WebGLRenderingContext): Mesh {
+    let vertex_buf = gl.createBuffer()!;
+    gl.bindBuffer(GL_ARRAY_BUFFER, vertex_buf);
+    gl.bufferData(GL_ARRAY_BUFFER, vertex_arr, GL_STATIC_DRAW);
+
+    let normal_buf = gl.createBuffer()!;
+    gl.bindBuffer(GL_ARRAY_BUFFER, normal_buf);
+    gl.bufferData(GL_ARRAY_BUFFER, normal_arr, GL_STATIC_DRAW);
+
+    let texcoord_buf = gl.createBuffer()!;
+    gl.bindBuffer(GL_ARRAY_BUFFER, texcoord_buf);
+    gl.bufferData(GL_ARRAY_BUFFER, texcoord_arr, GL_STATIC_DRAW);
+
+    let index_buf = gl.createBuffer()!;
+    gl.bindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buf);
+    gl.bufferData(GL_ELEMENT_ARRAY_BUFFER, index_arr, GL_STATIC_DRAW);
+
+    return {
+        VertexBuffer: vertex_buf,
+        VertexArray: vertex_arr,
+        NormalBuffer: normal_buf,
+        TexCoordBuffer: texcoord_buf,
+        IndexBuffer: index_buf,
+        IndexArray: index_arr,
+        IndexCount: index_arr.length,
+    };
+}
+
+// prettier-ignore
+let vertex_arr = Float32Array.from([
+    0.5,
+    0,
+    0.5,
+    -0.5,
+    0,
+    -0.5,
+    -0.5,
+    0,
+    0.5,
+    0.5,
+    0,
+    -0.5,
+]);
+
+// prettier-ignore
+let normal_arr = Float32Array.from([
+    0,
+    1,
+    0,
+    0,
+    1,
+    0,
+    0,
+    1,
+    0,
+    0,
+    1,
+    0,
+]);
+
+// prettier-ignore
+let texcoord_arr = Float32Array.from([
+    1,
+    0,
+    0,
+    1,
+    0,
+    0,
+    1,
+    1,
+]);
+
+// prettier-ignore
+let index_arr = Uint16Array.from([
+    1,
+    3,
+    0,
+    2,
+    1,
+    0,
+]);
