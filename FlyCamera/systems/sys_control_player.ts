8c13626f75933b2e9bc9681044b327071d67cfbf
Staś Małolepszy <stas@piesku.com>
2020-07-08 19:16:55 +0200

    (FlyCamera) Ignore RotationSpeed when using mouse

--- FlyCamera/systems/sys_control_player.ts
+++ FlyCamera/systems/sys_control_player.ts
@@ -1,11 +1,12 @@
-import {Vec3} from "../../common/math.js";
-import {from_axis} from "../../common/quat.js";
+import {DEG_TO_RAD, Quat, Vec3} from "../../common/math.js";
+import {from_axis, multiply} from "../../common/quat.js";
 import {Entity, Game} from "../game.js";
 import {Has} from "../world.js";
 
-const QUERY = Has.Move | Has.ControlPlayer;
+const QUERY = Has.Transform | Has.Move | Has.ControlPlayer;
 const AXIS_X = <Vec3>[1, 0, 0];
 const AXIS_Y = <Vec3>[0, 1, 0];
+const rotation: Quat = [0, 0, 0, 0];
 
 export function sys_control_player(game: Game, delta: number) {
     for (let i = 0; i < game.World.Mask.length; i++) {
@@ -39,22 +40,30 @@ function update(game: Game, entity: Entity, delta: number) {
     }
 
     if (control.Yaw && game.InputDelta.MouseX) {
-        let yaw_delta = game.InputDelta.MouseX * control.Yaw * delta;
-        if (yaw_delta !== 0) {
-            let move = game.World.Move[entity];
-            // Yaw is applied relative to the entity's local space, so that the
-            // Y axis is not affected by its current orientation.
-            move.LocalRotations.push(from_axis([0, 0, 0, 0], AXIS_Y, -yaw_delta));
-        }
+        let transform = game.World.Transform[entity];
+        // Scale the mouse input by the sensitivity.
+        let amount = game.InputDelta.MouseX * control.Yaw;
+        // Treat the pixels traveled by the mouse in this frame as literal Euler
+        // angles. This happens to work well and gives the mouse control an
+        // instant and precise feel. Note that the rotation won't be passed to
+        // sys_move; instead it's applied here and ignores Move.RotationSpeed on
+        // purpose.
+        from_axis(rotation, AXIS_Y, -amount * DEG_TO_RAD);
+
+        // Yaw is pre-multiplied, i.e. applied relative to the entity's local
+        // space; the Y axis is not affected by its current orientation.
+        multiply(transform.Rotation, rotation, transform.Rotation);
+        transform.Dirty = true;
     }
 
     if (control.Pitch && game.InputDelta.MouseY) {
-        let pitch_delta = game.InputDelta.MouseY * control.Pitch * delta;
-        if (pitch_delta !== 0) {
-            let move = game.World.Move[entity];
-            // Pitch is applied relative to the entity's self space, so that the
-            // X axis is always aligned with its left and right sides.
-            move.SelfRotations.push(from_axis([0, 0, 0, 0], AXIS_X, pitch_delta));
-        }
+        let transform = game.World.Transform[entity];
+        let amount = game.InputDelta.MouseY * control.Pitch;
+        from_axis(rotation, AXIS_X, amount * DEG_TO_RAD);
+
+        // Pitch is post-multiplied, i.e. applied relative to the entity's self
+        // space; the X axis is always aligned with its left and right sides.
+        multiply(transform.Rotation, transform.Rotation, rotation);
+        transform.Dirty = true;
     }
 }
