46d323ba1d72083ffb959f3697dafc93936dcfb9
Staś Małolepszy <stas@piesku.com>
2020-08-28 14:25:41 +0200

    (common/number) Add map_range

--- common/number.ts
+++ common/number.ts
@@ -5,3 +5,13 @@ export function lerp(from: number, to: number, progress: number) {
 export function clamp(min: number, max: number, num: number) {
     return Math.max(min, Math.min(max, num));
 }
+
+export function map_range(
+    value: number,
+    old_min: number,
+    old_max: number,
+    new_min: number,
+    new_max: number
+) {
+    return ((value - old_min) / (old_max - old_min)) * (new_max - new_min) + new_min;
+}
