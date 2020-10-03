ce6c51046656d815b3c5f36b6d2f8dfda4014f7d
Staś Małolepszy <stas@piesku.com>
2020-10-03 09:49:07 +0200

    Remove WebVR
    The WebVR example has historical value but it's also a maintence burden.

--- WebVR/blueprints/blu_viewer.ts
+++
@@ -1,9 +0,0 @@
-import {camera_vr} from "../components/com_camera.js";
-import {Blueprint} from "../core.js";
-import {Game} from "../game.js";
-
-export function blueprint_viewer(game: Game): Blueprint {
-    return {
-        Using: [camera_vr()],
-    };
-}
