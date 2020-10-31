4e6ca7635b4490b32d2c46aaf387d97e47f7e0fd
Staś Małolepszy <stas@piesku.com>
2020-10-31 13:51:50 +0100

    (common/{mat4,quat,vec3}) Add set, copy

--- common/mat4.ts
+++ common/mat4.ts
@@ -43,6 +43,26 @@ export function set(
     return out;
 }
 
+export function copy(out: Mat4, a: Mat4) {
+    out[0] = a[0];
+    out[1] = a[1];
+    out[2] = a[2];
+    out[3] = a[3];
+    out[4] = a[4];
+    out[5] = a[5];
+    out[6] = a[6];
+    out[7] = a[7];
+    out[8] = a[8];
+    out[9] = a[9];
+    out[10] = a[10];
+    out[11] = a[11];
+    out[12] = a[12];
+    out[13] = a[13];
+    out[14] = a[14];
+    out[15] = a[15];
+    return out;
+}
+
 export function invert(out: Mat4, a: Mat4) {
     let a00 = a[0],
         a01 = a[1],
