e6a5b2f0d2ee4985b2931b66cc3a41784d369a6e
Staś Małolepszy <stas@piesku.com>
2021-03-13 12:47:12 +0100

    (Particles) Use array blueprints

--- Particles/scenes/sce_stage.ts
+++ Particles/scenes/sce_stage.ts
@@ -1,7 +1,8 @@
 import {blueprint_camera} from "../blueprints/blu_camera.js";
 import {blueprint_flame_colored} from "../blueprints/blu_flame_colored.js";
 import {blueprint_flame_textured} from "../blueprints/blu_flame_textured.js";
-import {instantiate3d} from "../entity.js";
+import {transform} from "../components/com_transform.js";
+import {instantiate} from "../entity.js";
 import {Game} from "../game.js";
 import {World} from "../world.js";
 
@@ -11,20 +12,17 @@ export function scene_stage(game: Game) {
     game.Gl.clearColor(0.9, 0.9, 0.9, 1);
 
     // Camera.
-    instantiate3d(game, {
-        Translation: [0, 3, 10],
-        ...blueprint_camera(game),
-    });
+    instantiate(game, [...blueprint_camera(game), transform([0, 3, 10], [0, 1, 0, 0])]);
 
     // Colored flame.
-    instantiate3d(game, {
-        Scale: [5, 1, 1],
+    instantiate(game, [
         ...blueprint_flame_colored(game),
-    });
+        transform(undefined, undefined, [5, 1, 1]),
+    ]);
 
     // Textured flame.
-    instantiate3d(game, {
-        Scale: [5, 1, 1],
+    instantiate(game, [
         ...blueprint_flame_textured(game),
-    });
+        transform(undefined, undefined, [5, 1, 1]),
+    ]);
 }
