aef53710d7bd477a16c4eb086b06db9ac3c43d07
Staś Małolepszy <stas@piesku.com>
2021-03-14 03:11:41 +0100

    (Texture) Use array blueprints

--- Texture/scenes/sce_stage.ts
+++ Texture/scenes/sce_stage.ts
@@ -3,7 +3,8 @@ import {control_move} from "../components/com_control_move.js";
 import {light_directional} from "../components/com_light.js";
 import {move} from "../components/com_move.js";
 import {render_textured_diffuse, render_textured_unlit} from "../components/com_render1.js";
-import {instantiate3d} from "../entity.js";
+import {transform} from "../components/com_transform.js";
+import {instantiate} from "../entity.js";
 import {Game} from "../game.js";
 import {World} from "../world.js";
 
@@ -12,42 +13,32 @@ export function scene_stage(game: Game) {
     game.ViewportResized = true;
 
     // Camera.
-    instantiate3d(game, {
-        Translation: [0, 0, 3],
-        ...blueprint_camera(game),
-    });
+    instantiate(game, [...blueprint_camera(game), transform([0, 0, 3], [0, 1, 0, 0])]);
 
     // Directional light.
-    instantiate3d(game, {
-        Translation: [1, 1, 1],
-        Using: [light_directional([1, 1, 1], 1)],
-    });
+    instantiate(game, [transform([1, 1, 1]), light_directional([1, 1, 1], 1)]);
 
     // Unlit kulka.
-    instantiate3d(game, {
-        Translation: [-1, 0, 0],
-        Using: [
-            control_move(null, [0, 1, 0, 0]),
-            move(0, 0.5),
-            render_textured_unlit(
-                game.MaterialTexturedUnlit,
-                game.MeshKulka,
-                game.Textures["checker1"]
-            ),
-        ],
-    });
+    instantiate(game, [
+        transform([-1, 0, 0]),
+        control_move(null, [0, 1, 0, 0]),
+        move(0, 0.5),
+        render_textured_unlit(
+            game.MaterialTexturedUnlit,
+            game.MeshKulka,
+            game.Textures["checker1"]
+        ),
+    ]);
 
     // Diffuse kulka.
-    instantiate3d(game, {
-        Translation: [1, 0, 0],
-        Using: [
-            control_move(null, [0, 1, 0, 0]),
-            move(0, 0.5),
-            render_textured_diffuse(
-                game.MaterialTexturedDiffuse,
-                game.MeshKulka,
-                game.Textures["checker1"]
-            ),
-        ],
-    });
+    instantiate(game, [
+        transform([1, 0, 0]),
+        control_move(null, [0, 1, 0, 0]),
+        move(0, 0.5),
+        render_textured_diffuse(
+            game.MaterialTexturedDiffuse,
+            game.MeshKulka,
+            game.Textures["checker1"]
+        ),
+    ]);
 }
