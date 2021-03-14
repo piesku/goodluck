13181e2c7e7ab76dc34115ccdcba6333de40603e
Staś Małolepszy <stas@piesku.com>
2021-03-14 17:13:28 +0100

    (ThirdPerson) Use array blueprints

--- ThirdPerson/blueprints/blu_player.ts
+++ ThirdPerson/blueprints/blu_player.ts
@@ -1,3 +1,4 @@
+import {children} from "../components/com_children.js";
 import {collide} from "../components/com_collide.js";
 import {control_player} from "../components/com_control_player.js";
 import {light_point} from "../components/com_light.js";
@@ -5,38 +6,31 @@ import {move} from "../components/com_move.js";
 import {named} from "../components/com_named.js";
 import {render_colored_diffuse} from "../components/com_render1.js";
 import {RigidKind, rigid_body} from "../components/com_rigid_body.js";
-import {Blueprint3D} from "../entity.js";
+import {transform} from "../components/com_transform.js";
+import {Blueprint} from "../entity.js";
 import {Game, Layer} from "../game.js";
 
-export function blueprint_player(game: Game): Blueprint3D {
-    return {
-        Rotation: [0, 1, 0, 0],
-        Using: [
-            control_player(true, 0.2, 0),
-            move(10, 3),
-            collide(true, Layer.Player, Layer.Terrain),
-            rigid_body(RigidKind.Dynamic),
-        ],
-        Children: [
-            {
-                // Body.
-                Using: [
-                    render_colored_diffuse(game.MaterialColoredDiffuseGouraud, game.MeshCube, [
-                        1,
-                        1,
-                        0.3,
-                        1,
-                    ]),
-                ],
-            },
-            {
-                // Camera rig anchor.
-                Using: [named("camera anchor"), move(0, 3), control_player(false, 0, 0.2)],
-            },
-            {
-                Translation: [0, 5, 0],
-                Using: [light_point([1, 1, 1], 5)],
-            },
-        ],
-    };
+export function blueprint_player(game: Game): Blueprint {
+    return [
+        control_player(true, 0.2, 0),
+        move(10, 3),
+        collide(true, Layer.Player, Layer.Terrain),
+        rigid_body(RigidKind.Dynamic),
+        children(
+            // Body.
+            [
+                transform(),
+                render_colored_diffuse(game.MaterialColoredDiffuseGouraud, game.MeshCube, [
+                    1,
+                    1,
+                    0.3,
+                    1,
+                ]),
+            ],
+            // Camera rig anchor.
+            [transform(), named("camera anchor"), move(0, 3), control_player(false, 0, 0.2)],
+            // Overhead light.
+            [transform([0, 5, 0]), light_point([1, 1, 1], 5)]
+        ),
+    ];
 }
--- ThirdPerson/scenes/sce_stage.ts
+++ ThirdPerson/scenes/sce_stage.ts
@@ -2,7 +2,8 @@ import {blueprint_camera_follow} from "../blueprints/blu_camera_follow.js";
 import {blueprint_ground} from "../blueprints/blu_ground.js";
 import {blueprint_player} from "../blueprints/blu_player.js";
 import {light_directional} from "../components/com_light.js";
-import {instantiate3d} from "../entity.js";
+import {transform} from "../components/com_transform.js";
+import {instantiate} from "../entity.js";
 import {Game} from "../game.js";
 import {World} from "../world.js";
 
@@ -15,25 +16,23 @@ export function scene_stage(game: Game) {
     for (let z = 0; z < map_size; z++) {
         for (let x = 0; x < map_size; x++) {
             // Ground.
-            instantiate3d(game, {
-                Translation: [tile_size * (x - map_size / 2), 0, tile_size * (z - map_size / 2)],
-                ...blueprint_ground(game, tile_size),
-            });
+            instantiate(game, [
+                ...blueprint_ground(game),
+                transform(
+                    [tile_size * (x - map_size / 2), 0, tile_size * (z - map_size / 2)],
+                    undefined,
+                    [tile_size, 1, tile_size]
+                ),
+            ]);
         }
     }
 
     // Directional light.
-    instantiate3d(game, {
-        Translation: [1, 1, 1],
-        Using: [light_directional([1, 1, 1], 0.2)],
-    });
+    instantiate(game, [transform([1, 1, 1]), light_directional([1, 1, 1], 0.2)]);
 
     // Player.
-    instantiate3d(game, {
-        ...blueprint_player(game),
-        Translation: [0, 1, 0],
-    });
+    instantiate(game, [...blueprint_player(game), transform([0, 1, 0], [0, 1, 0, 0])]);
 
     // Camera.
-    instantiate3d(game, blueprint_camera_follow(game));
+    instantiate(game, [...blueprint_camera_follow(game), transform([0, 1000, 1000], [0, 1, 0, 0])]);
 }
