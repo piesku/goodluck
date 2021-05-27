20cc295339935b81eca93f76df35c27469fc8d36
Staś Małolepszy <stas@piesku.com>
2021-05-27 15:28:41 +0200

    (common/material) Expose NormalArray and TexCoordArray in Mesh

--- common/material.ts
+++ common/material.ts
@@ -4,7 +4,9 @@ export interface Mesh {
     VertexBuffer: WebGLBuffer;
     VertexArray: Float32Array;
     NormalBuffer: WebGLBuffer;
+    NormalArray: Float32Array;
     TexCoordBuffer: WebGLBuffer;
+    TexCoordArray: Float32Array;
     IndexBuffer: WebGLBuffer;
     IndexArray: Uint16Array;
     IndexCount: number;
