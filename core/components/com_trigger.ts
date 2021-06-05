1bfc3d7b6987bd07c6a3a7510b5d0e9b9f8102c3
Staś Małolepszy <stas@piesku.com>
2021-06-05 19:10:10 +0200

    (Trigger) Add Trigger.Mask

--- core/components/com_trigger.ts
+++ core/components/com_trigger.ts
@@ -1,16 +1,18 @@
 import {Action} from "../actions.js";
-import {Entity, Game} from "../game.js";
+import {Entity, Game, Layer} from "../game.js";
 import {Has} from "../world.js";
 
 export interface Trigger {
+    Mask: Layer;
     Action: Action;
 }
 
-export function trigger(Action: Action) {
+export function trigger(mask: Layer, action: Action) {
     return (game: Game, entity: Entity) => {
         game.World.Signature[entity] |= Has.Trigger;
         game.World.Trigger[entity] = {
-            Action,
+            Mask: mask,
+            Action: action,
         };
     };
 }
