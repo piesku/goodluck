c9db3b43362239dd6c8ecd13ce83764d853eda8c
Staś Małolepszy <stas@piesku.com>
2021-06-12 19:59:17 +0200

    Minimal3D extends GameWebGL1

--- Minimal3D/game.ts
+++ Minimal3D/game.ts
@@ -1,9 +1,7 @@
-import {GL_CULL_FACE, GL_DEPTH_TEST} from "../common/webgl.js";
+import {GameWebGL1} from "../common/game.js";
 import {mat1_forward_colored_gouraud} from "../materials/mat1_forward_colored_gouraud.js";
 import {mesh_cube} from "../meshes/cube.js";
-import {frame_reset, frame_setup, input_init, input_pointer_lock, loop_init} from "./impl.js";
 import {sys_camera} from "./systems/sys_camera.js";
-import {sys_framerate} from "./systems/sys_framerate.js";
 import {sys_light} from "./systems/sys_light.js";
 import {sys_render_forward} from "./systems/sys_render1_forward.js";
 import {sys_resize} from "./systems/sys_resize.js";
@@ -12,23 +10,8 @@ import {World} from "./world.js";
 
 export type Entity = number;
 
-export class Game {
+export class Game extends GameWebGL1 {
     World = new World();
-
-    ViewportWidth = window.innerWidth;
-    ViewportHeight = window.innerHeight;
-    ViewportResized = true;
-
-    InputState: Record<string, number> = {};
-    InputDelta: Record<string, number> = {};
-    InputDistance: Record<string, number> = {};
-    InputTouches: Record<string, number> = {};
-
-    Ui = document.querySelector("main")!;
-    Billboard = document.querySelector("#billboard")! as HTMLCanvasElement;
-    Canvas = document.querySelector("#scene")! as HTMLCanvasElement;
-    Gl = this.Canvas.getContext("webgl")!;
-    ExtVao = this.Gl.getExtension("OES_vertex_array_object")!;
 
     MaterialColoredGouraud = mat1_forward_colored_gouraud(this.Gl);
     MeshCube = mesh_cube(this.Gl);
@@ -38,26 +21,11 @@ export class Game {
     LightDetails = new Float32Array(4 * 8);
     Cameras: Array<Entity> = [];
 
-    constructor() {
-        loop_init(this);
-        input_init(this);
-        input_pointer_lock(this);
-
-        this.Gl.enable(GL_DEPTH_TEST);
-        this.Gl.enable(GL_CULL_FACE);
-    }
-
-    FrameUpdate(delta: number) {
-        frame_setup(this);
-        let now = performance.now();
-
+    override FrameUpdate(delta: number) {
         sys_transform(this, delta);
         sys_resize(this, delta);
         sys_camera(this, delta);
         sys_light(this, delta);
         sys_render_forward(this, delta);
-
-        sys_framerate(this, delta, performance.now() - now);
-        frame_reset(this);
     }
 }
