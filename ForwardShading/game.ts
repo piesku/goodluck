e59394885db89fe85c612e77c94a4093188f750d
Staś Małolepszy <stas@piesku.com>
2021-07-14 22:19:31 +0200

    (ForwardShading) Migrate to WebGL2

--- ForwardShading/game.ts
+++ ForwardShading/game.ts
@@ -1,39 +1,39 @@
-import {GameWebGL1} from "../common/game.js";
+import {GameWebGL2} from "../common/game.js";
 import {Entity} from "../common/world.js";
-import {mat1_forward_colored_gouraud} from "../materials/mat1_forward_colored_gouraud.js";
-import {mat1_forward_colored_phong} from "../materials/mat1_forward_colored_phong.js";
-import {mat1_forward_colored_points} from "../materials/mat1_forward_colored_points.js";
+import {mat2_forward_colored_gouraud} from "../materials/mat2_forward_colored_gouraud.js";
+import {mat2_forward_colored_phong} from "../materials/mat2_forward_colored_phong.js";
+import {mat2_forward_colored_points} from "../materials/mat2_forward_colored_points.js";
 import {
-    mat1_forward_colored_unlit,
-    mat1_forward_colored_wireframe,
-} from "../materials/mat1_forward_colored_unlit.js";
-import {mat1_forward_mapped_shaded} from "../materials/mat1_forward_mapped_shaded.js";
-import {mat1_forward_textured_gouraud} from "../materials/mat1_forward_textured_gouraud.js";
-import {mat1_forward_textured_phong} from "../materials/mat1_forward_textured_phong.js";
-import {mat1_forward_textured_unlit} from "../materials/mat1_forward_textured_unlit.js";
+    mat2_forward_colored_unlit,
+    mat2_forward_colored_wireframe,
+} from "../materials/mat2_forward_colored_unlit.js";
+import {mat2_forward_mapped_shaded} from "../materials/mat2_forward_mapped_shaded.js";
+import {mat2_forward_textured_gouraud} from "../materials/mat2_forward_textured_gouraud.js";
+import {mat2_forward_textured_phong} from "../materials/mat2_forward_textured_phong.js";
+import {mat2_forward_textured_unlit} from "../materials/mat2_forward_textured_unlit.js";
 import {mesh_cube} from "../meshes/cube.js";
 import {mesh_icosphere_smooth} from "../meshes/icosphere_smooth.js";
 import {sys_camera} from "./systems/sys_camera.js";
 import {sys_control_always} from "./systems/sys_control_always.js";
 import {sys_light} from "./systems/sys_light.js";
 import {sys_move} from "./systems/sys_move.js";
-import {sys_render_forward} from "./systems/sys_render1_forward.js";
+import {sys_render_forward} from "./systems/sys_render2_forward.js";
 import {sys_resize} from "./systems/sys_resize.js";
 import {sys_transform} from "./systems/sys_transform.js";
 import {World} from "./world.js";
 
-export class Game extends GameWebGL1 {
+export class Game extends GameWebGL2 {
     World = new World();
 
-    MaterialColoredPoints = mat1_forward_colored_points(this.Gl);
-    MaterialColoredWireframe = mat1_forward_colored_wireframe(this.Gl);
-    MaterialColoredUnlit = mat1_forward_colored_unlit(this.Gl);
-    MaterialColoredGouraud = mat1_forward_colored_gouraud(this.Gl);
-    MaterialColoredPhong = mat1_forward_colored_phong(this.Gl);
-    MaterialTexturedUnlit = mat1_forward_textured_unlit(this.Gl);
-    MaterialTexturedGouraud = mat1_forward_textured_gouraud(this.Gl);
-    MaterialTexturedPhong = mat1_forward_textured_phong(this.Gl);
-    MaterialMapped = mat1_forward_mapped_shaded(this.Gl);
+    MaterialColoredPoints = mat2_forward_colored_points(this.Gl);
+    MaterialColoredWireframe = mat2_forward_colored_wireframe(this.Gl);
+    MaterialColoredUnlit = mat2_forward_colored_unlit(this.Gl);
+    MaterialColoredGouraud = mat2_forward_colored_gouraud(this.Gl);
+    MaterialColoredPhong = mat2_forward_colored_phong(this.Gl);
+    MaterialTexturedUnlit = mat2_forward_textured_unlit(this.Gl);
+    MaterialTexturedGouraud = mat2_forward_textured_gouraud(this.Gl);
+    MaterialTexturedPhong = mat2_forward_textured_phong(this.Gl);
+    MaterialMapped = mat2_forward_mapped_shaded(this.Gl);
 
     MeshCube = mesh_cube(this.Gl);
     MeshIcosphereSmooth = mesh_icosphere_smooth(this.Gl);
