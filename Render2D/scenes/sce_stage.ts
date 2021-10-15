19a88203009f0a03100eed9ae902d4b626d339a3
Staś Małolepszy <stas@piesku.com>
2021-10-15 17:29:41 +0200

    Place on Z from closest to farthest to avoid overdraw

--- Render2D/scenes/sce_stage.ts
+++ Render2D/scenes/sce_stage.ts
@@ -13,12 +13,14 @@ export function scene_stage(game: Game) {
     game.ViewportResized = true;
 
     // Camera.
-    instantiate(game, [...blueprint_camera(game), transform([0, 0, 5], [0, 1, 0, 0])]);
+    instantiate(game, [...blueprint_camera(game), transform([0, 0, 2], [0, 1, 0, 0])]);
 
     for (let i = 0; i < game.InstanceCount; i++) {
         instantiate(game, [
             transform(
-                [float() * 2 - 1, float() * 2 - 1, float() * 2 - 1],
+                // Place entities on the Z axis from closest to the farthest away
+                // to avoid overdraw.
+                [float() * 2 - 1, float() * 2 - 1, -i / game.InstanceCount],
                 from_euler([0, 0, 0, 1], 0, 0, float() * 360),
                 [0.05, 0.05, 1]
             ),
