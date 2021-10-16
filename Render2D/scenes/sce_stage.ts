bb0b3fa8fe11d9ea3d586260b0b4f81622b39dc3
Staś Małolepszy <stas@piesku.com>
2021-10-16 14:42:16 +0200

    (Render2D) Remove the camera blueprint

--- Render2D/scenes/sce_stage.ts
+++ Render2D/scenes/sce_stage.ts
@@ -2,7 +2,8 @@ import {hsva_to_vec4} from "../../common/color.js";
 import {instantiate} from "../../common/game.js";
 import {from_euler} from "../../common/quat.js";
 import {float} from "../../common/random.js";
-import {blueprint_camera} from "../blueprints/blu_camera.js";
+import {camera_forward_ortho} from "../components/com_camera.js";
+import {children} from "../components/com_children.js";
 import {render2d} from "../components/com_render2d.js";
 import {transform} from "../components/com_transform.js";
 import {Game} from "../game.js";
@@ -13,7 +14,10 @@ export function scene_stage(game: Game) {
     game.ViewportResized = true;
 
     // Camera.
-    instantiate(game, [...blueprint_camera(game), transform([0, 0, 2], [0, 1, 0, 0])]);
+    instantiate(game, [
+        transform([0, 0, 2], [0, 1, 0, 0]),
+        children([transform(undefined, [0, 1, 0, 0]), camera_forward_ortho(2, 1, 100)]),
+    ]);
 
     for (let i = 0; i < game.InstanceCount; i++) {
         instantiate(game, [
