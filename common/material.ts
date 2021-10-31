5eddf83a81a388d45cb4cfb3c0748d3e3f97f789
Staś Małolepszy <stas@piesku.com>
2021-10-31 18:33:11 +0100

    Revert "Merge pull request #106 from piesku/esbuild"
    This reverts commit ab434ccd334dbebca99078da8ad5e7456164d42d, reversing
    changes made to 400a6dc99866e69d79415fcf186084ce18ef4780.
    
    Whoa, esbuild doesn't support const enums at all.

--- common/material.ts
+++ common/material.ts
@@ -12,7 +12,7 @@ export function link(gl: WebGLRenderingContext, vertex: string, fragment: string
     gl.attachShader(program, compile(gl, GL_FRAGMENT_SHADER, fragment));
     gl.linkProgram(program);
 
-    if (DEBUG && !gl.getProgramParameter(program, GL_LINK_STATUS)) {
+    if (!gl.getProgramParameter(program, GL_LINK_STATUS)) {
         throw new Error(gl.getProgramInfoLog(program)!);
     }
 
@@ -24,7 +24,7 @@ function compile(gl: WebGLRenderingContext, type: GLenum, source: string) {
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
 
-    if (DEBUG && !gl.getShaderParameter(shader, GL_COMPILE_STATUS)) {
+    if (!gl.getShaderParameter(shader, GL_COMPILE_STATUS)) {
         throw new Error(gl.getShaderInfoLog(shader)!);
     }
 
